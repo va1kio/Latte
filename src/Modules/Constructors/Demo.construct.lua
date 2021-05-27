f9297acbca54c4266470b75b7d99099d005c9d32
va1kio <40730127+va1kio@users.noreply.github.com>
2021-05-27 14:53:40 +0800

    More demo

--- src/Modules/Constructors/Demo.construct.lua
+++ src/Modules/Constructors/Demo.construct.lua
@@ -1,14 +1,27 @@
 local module = {}
 
 function module:Initialize()
-	self._Signal = self.Core.Signals.new("CoronavirusCured")
-	self._Signal:Register(function(CountriesCured)
-		self.Core.println(table.concat(CountriesCured, ", "))
+	self._Signal = self.Core.Signals.new("Demo")
+	self._Signal:Register(function(Argument)
+		local collection = self.Core.Collections:Get(Argument)
+		self.Core.println("Got " .. Argument)
+		self.Core.SetTimeout(function()
+			self.Core.println(table.concat(collection:Get("Original").Value, ", "))
+			self.Core.println(table.concat(collection:Get("SortedResult").Value, ", "))
+		end, 5)
 	end)
 end
 
 function module:Start()
-	self._Signal:Emit({"USA", "Japan", "UK", "China", "Russia"})
+	local collection = self.Core.Collections.new(script.Name)
+	local demoTable = {}
+	for _ = 1, 150 do
+		table.insert(demoTable, math.random(1, 9999))
+	end
+
+	collection:Append("Original", table.pack(table.unpack(demoTable)))
+	collection:Append("SortedResult", self.Core.Sort.Bubble(demoTable))
+	self._Signal:Emit(collection.Name)
 end
 
 return module

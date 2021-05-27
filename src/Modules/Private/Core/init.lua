00b9565e8a0e0d2ae3ac026d86816985ead4e3b9
va1kio <40730127+va1kio@users.noreply.github.com>
2021-05-27 14:53:49 +0800

    Cleanups

--- src/Modules/Private/Core/init.lua
+++ src/Modules/Private/Core/init.lua
@@ -7,8 +7,8 @@ Core.Out = {
 }
 
 function Core:Import(ModuleName, Category)
-	if self.Out[Category] then
-		return self.Out[Category][ModuleName]
+	if Core.Out[Category] then
+		return Core.Out[Category][ModuleName]
 	end
 end
 
@@ -24,12 +24,11 @@ function Core.error(...)
 	error("Latte; " .. ..., debug.traceback)
 end
 
-for _, possiblyModule in ipairs(script.Parent:GetChildren()) do
+for _, possiblyModule in ipairs(script:GetChildren()) do
 	if possiblyModule:IsA("ModuleScript") and possiblyModule ~= script then
 		Core[possiblyModule.Name] = require(possiblyModule)
 	end
 end
 
 Core.Out = setmetatable({}, Core.Out)
-Core.__index = Core
-return setmetatable({}, Core)
+return Core

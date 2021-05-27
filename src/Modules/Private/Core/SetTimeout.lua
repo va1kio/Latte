176ec4643263968dd961e7ceba5fdb0974e34b2d
nanako <nanako.matsumooo@outlook.jp>
2021-05-27 12:16:16 +0800

    Add SetTimeout.lua

---
+++ src/Modules/Private/Core/SetTimeout.lua
@@ -0,0 +1,28 @@
+local RunService = game.GetService(game, "RunService")
+
+local _yieldables = {}
+
+RunService.Stepped:Connect(function()
+	local clock = os.clock()
+	for index, item in ipairs(_yieldables) do
+		local difference = clock - item.Timestamp
+		if difference >= item.Interval then
+			table.remove(_yieldables, index)
+			coroutine.resume(item.Thread, difference, clock)
+		end
+	end	
+end)
+
+return function(Callback, Interval)
+	if type(Interval) ~= "number" or Interval < 0 then
+		Interval = 0	
+	end
+	
+	table.insert(_yieldables, {
+		["Timestamp"] = os.clock,
+		["Interval"] = Interval,
+		["Thread"] = coroutine.running
+	}
+	coroutine.yield()
+	Callback()
+end
